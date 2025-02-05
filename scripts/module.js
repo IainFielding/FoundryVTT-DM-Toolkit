12163de37f4b1126b65f89989c0a655ad3975ccb
Iain Fielding <iain@iainfielding.com>
2025-02-05 11:18:42 +0000

    Addion bastion tab at level 1

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,10 @@
 Hooks.once('init', async function() {
-
+    foundry.utils.mergeObject(CONFIG.DND5E.facilities, {
+        advancement: {
+          basic: { 0: 2 },
+          special: { 0: 2 }
+        }
+      })
 });
 
 Hooks.once('ready', async function() {
